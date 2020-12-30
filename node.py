280890e7d12de614ef1288a468f2808a26fbca67
Paul Cesaretti <paul.cesaretti@gmail.com>
2020-12-29 22:03:21 -0500

    Add files via upload

--- node.py
+++ node.py
@@ -1,3 +1,142 @@
+<<<<<<< HEAD
+import threading
+import socket
+import queue
+
+from utilities import *
+
+
+#incoming and outgoing queue are managed by other threads. 
+
+
+class Node(threading.Thread):
+
+    def __init__(self, _id, edges, links):
+        #extending the thread class
+        super().__init__()
+
+        #identifer
+        self._id = _id
+
+        #state variables
+        self.coord_so_far = None
+        self.port_to_coord = None
+        self.status = 'idle'
+        recd_reply = {}
+
+        #connections
+        self.edges = edges
+        self.links = links
+        #self.ports
+
+    def run(self):
+        print(self._id)
+        print(self.edges)
+        print(self.links)
+
+    #def assign_edge(self):
+
+    #def set_of_ports(self):
+
+    #def get_port(self):
+
+    #port_to(self):
+
+    #sender_of(self):
+
+    #remove_edge(self):
+
+    #reconfig(self):
+
+
+#the Incoming_Msg_Cntrll will act as a server listening 
+#for messages that are 
+class Incoming_Msg_Cntrll(threading.Thread):
+
+    def __init__(self, incoming_queue, port, host):
+        super().__init__()
+        self.in_q = incoming_queue
+
+        
+        #construct a socket and listen for incoming messages
+        in_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        in_socket.bind(host, port)
+        in_socket.listen()
+
+
+    #get a message and put it into the queue.
+    def run(self):
+        #while True:
+
+        #establish the connection with the client seeking to send info
+        c, addr = in_socket.accept()
+        message = c.recv(1024)
+
+        #FOR TESTING
+        if message == 'hello':
+            c.sendall('hello')
+
+        #if the message is either failure or reconfig
+        self.in_q.put(message) 
+
+
+
+#checks the incoming queue for a node to see if there are any
+# messages to forward
+class Sending_Msg_Cntrll(threading.Thread):
+
+    def __init__(self, incoming_queue, outgoing_queue, port, host):
+        super().__init__()
+
+        self.in_queue = incoming_queue
+        self.out_queue = outgoing_queue
+        self.port = port
+        self.host = host
+
+        #socket object to send messages
+        self.socket = None
+
+    def run(self):
+        #check if there are messages to send
+        if  
+
+
+
+
+if __name__ == '__main__':        
+
+    #initialize the queues for the node which 
+    #are managed by the receiver thread and the sender thread
+    incoming_queue = queue.Queue()
+    outgoing_queue = queue.Queue()
+
+
+    #get the file from the command line.
+    parser = opt_parser()
+    options = parser.parse_args() 
+
+
+    #load the network topology which holds port, host, 
+    #links and edges information for a given node. 
+    
+    ntwrk_cnfg = load_network_topology(options.topology)
+
+    #populate this node with its id, edges, links, port and host information
+    _id = options.id
+    host = ntwrk_cnfg[_id]['host']
+    port = ntwrk_cnfg[_id]['port']
+    links = ntwrk_cnfg[_id]['links']
+    edges = ntwrk_cnfg[_id]['edges']
+
+    #initialize the node with its host, links and edges
+    node = Node(_id,edges,links)
+
+
+    #send a message between (two) three nodes
+    #node.start()
+
+
+=======
 #node.py abstracts the functionality of a computing agent
 #in the network. It is divided into three main threads:
 # 1) a Receiver class to manage messages coming into the node
@@ -84,4 +223,5 @@ class Node(threading.Thread):
         self._edges 
 
         
+>>>>>>> 64231148f7e43445a0f79f5e4cb260b3fd4c732f
 
--- utilities.py
+++ utilities.py
@@ -1,5 +1,17 @@
 import json
 import socket
+<<<<<<< HEAD
+import argparse
+
+#read arguments from the command line
+def opt_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', default=15, type=int)
+    parser.add_argument('--topology', default='./ntwk_config.json', type=str)
+    return parser
+
+=======
+>>>>>>> 64231148f7e43445a0f79f5e4cb260b3fd4c732f
 
 #Function to read the network topology from 
 #the json file
